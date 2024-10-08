fc7d1ba88118e555148da3cbc8c4540bfc1e9271
hindusravani <nhindusravani@gmail.com>
2024-10-08 16:18:47 +0530

    Initial commit with project files

--- core/src/main/java/com/adobe/aem/guides/demo/core/models/Assignment.java
+++ core/src/main/java/com/adobe/aem/guides/demo/core/models/Assignment.java
@@ -1,11 +1,8 @@
 package com.adobe.aem.guides.demo.core.models;
-
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
-
 import javax.annotation.PostConstruct;
-
 import org.apache.sling.api.SlingHttpServletRequest;
 import org.apache.sling.api.resource.Resource;
 import org.apache.sling.api.resource.ResourceResolver;
@@ -13,9 +10,7 @@ import org.apache.sling.models.annotations.DefaultInjectionStrategy;
 import org.apache.sling.models.annotations.Model;
 import org.apache.sling.models.annotations.injectorspecific.SlingObject;
 import org.apache.sling.models.annotations.injectorspecific.ValueMapValue;
-
 import com.day.cq.wcm.api.Page;
-
 @Model(adaptables={Resource.class,SlingHttpServletRequest.class},defaultInjectionStrategy=DefaultInjectionStrategy.OPTIONAL)
 public class Assignment {
     @ValueMapValue
